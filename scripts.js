156d0b482aa24854b66ad17e6c130ce6a164261f
Nathan <PaperNathan@gmail.com>
2018-04-04 03:51:33 +0800

    first commit

---
+++ scripts.js
@@ -0,0 +1,87 @@
+// Global Variable Declarations
+let testEN = [
+    "apple",
+    "ball",
+    "cat",
+    "dog",
+    "elephant",
+    "frog",
+    "apple",
+    "ball",
+    "cat",
+    "dog",
+    "elephant",
+    "frog"
+],
+
+testCN = [
+    "蘋果",
+    "球",
+    "貓",
+    "狗",
+    "大象",
+    "青蛙",
+    "蘋果",
+    "球",
+    "貓",
+    "狗",
+    "大象",
+    "青蛙"
+];
+
+combineVocab(testEN, testCN);
+
+// Takes 2 arrays English Words and Chinse Words and combines them into 1 object
+// with the key being English and the value being Chinese
+function combineVocab(en, cn) {
+    let newTest = {};
+    let toggle = true;
+
+    // Object Builder
+    for (i=0; i<en.length; i++) {
+        newTest[en[i]] = cn[i];
+    }
+
+    buildPDF(newTest);  // Sends this new object to be constructed
+}
+
+function buildPDF(vocab) {
+    let doc = new jsPDF('landscape');
+    let runTime = Math.ceil(vocab.length / 6);
+    let keysArray = Object.keys(vocab),
+        valuesArray = Object.values(vocab);
+
+    for (i=0; i<runTime; i++) {
+        for (j=i; j<i+6; j++) {
+            setsOfSix(doc, keysArray, j);
+        }
+        for (h=i; h<i+6; h++) {
+            setsOfSix(doc, valuesArray, h);
+        }
+    }
+
+    doc.output('save', 'Flash-Cards.pdf');
+    
+}
+
+function setsOfSix(doc, wordList, i) {
+    let x, y;  // mm for A4
+
+    console.log(wordList);
+
+    if (i < 3) {
+        y = 50;
+        x = 50 + (99*i) - wordList[i].length;
+    } else {
+        y = 155;
+        x = 50 + (99*(i-3)) - wordList[i].length;
+    }
+    
+    doc.text(x, y, wordList[i]);
+    
+    if (i == 5) {
+        doc.addPage();
+    }
+    
+    return doc;
+}
